12453197f9f38e5a395fe31c6c939dc34ac60b2f
Lee <adjh54ir@gmail.com>
2022-07-05 17:56:35 +0900

    최종 수정사항 커밋

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # python_face_recognition
-Python의 face_recognition 오픈소스를 기반으로 얼굴 탐지 기능 
+
+> `Python의 face_recognition 오픈소스를 기반으로 얼굴 탐지 기능` 
+
+<br>
+
+# 1) cmake 설치 가이드 
+
+> CMake란 여러 환경(window , linux 등)에 맞는 build process를 작성한 것으로, 
+CMakeLists.txt에 기술된 내용을 바탕으로 각 확경에 맞는 Makefile을 생성해 주는 빌드 프로그램이다. 
+CMake를 통해 Makefile을 만들게 되면, 추가적인 파일이 추가되지 않는 한 Makefile을 수정하지 않고, 자동으로 생성해준다는 장점이 있지만,
+단순히 Makefile을 생성해 주는 것이기 때문에 Make를 추가로 해야한다.
+
+
+    # default pip install 
+    $ pip install --upgrade pip
+
+    # install cmake 
+    $ brew install cmake
+
+    # cmake version 3.23.2
+    $ cmake --version 
+
+    $ pip install cmake
+
+
+# 2) dlib 설치 가이드
+> C++로 실제 기계 학습 및 데이터 분석 애플리케이션을 만들기 위한 툴킷
+
+    # git clone
+    $ git clone <https://github.com/davisking/dlib.git>
+
+    $ cd dlib
+
+    $ mkdir build
+
+    $ cd build
+
+    # 해당 부분에서 에러 발생 하단 참고!
+    $ cmake ..
+
+    $ cmake --build .
+
+    $ cd ..
+
+    $ sudo python3 setup.py install
+
+ 
+<br>
+    
+> cmake .. 에 대한 에러 발생
+
+    CMake Error at /usr/local/Cellar/cmake/3.23.2/share/cmake/Modules/CMakeTestCCompiler.cmake:69 (message):
+    The C compiler
+
+    "/usr/bin/cc"
+
+    is not able to compile a simple test program.
+
+    It fails with the following output:
+
+    Change Dir: /Users/lee/Desktop/workspace/WHATNEED/dlib/build/CMakeFiles/CMakeTmp
+
+    Run Build Command(s):/usr/bin/make -f Makefile cmTC_e7b91/fast && xcrun: error: active developer path ("/Applications/Xcode.app/Contents/Developer") does not exist
+    Use `sudo xcode-select --switch path/to/Xcode.app` to specify the Xcode that you wish to use for command line developer tools, or use `xcode-select --install` to install the standalone command line developer tools.
+    See `man xcode-select` for more details.
+
+    CMake will not be able to correctly generate this project.
+    Call Stack (most recent call first):
+    CMakeLists.txt:3 (project)
+
+<br> 
+
+> 해결책
+<br>
+
+
+    # xcode-select version 2395.
+    $ xcode-select --version
+
+    $ sudo xcode-select --reset
+
+# 3) face_recognition 라이브러리 설치
+
+    $ pip install face_recognition
+
+    # Reference 
+    $ pip install -r requirements.txt
+    $ pip install -r requirements_dev.txt
+
+# 4) 기타 라이브러리 설치
+
+    $ pip install opencv-python
+
+
+ 
--- main_modules/main.py
+++ main_modules/main.py
@@ -4,16 +4,17 @@ import cv2
 import numpy as np
 
 """
-    스트림으로 측정되는 얼굴이 등록된 얼굴인지 여부를 확인하는 모듈 
+    [Modules] 스트림으로 측정 되는 얼굴이 등록된 얼굴 인지 여부를 확인 하는 모듈
+    - 존재 할 경우 파일명으로 이름이 그려지고
+    - 존재하지 않을 경우 Unknown으로 이름이 그려집니다. 
 """
 
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 # STEP1: 저장된 이미지 모든 리스트(ALL)를 가져온다.
-# TODO: 추후 S3 혹은 저장된 공간에서 이미지를 가져옴.
+# TODO: 추후 S3 혹은 저장된 공간에서 이미지를 가져 올 예정.
 images = os.listdir("images")
-print(os.getcwd())
 print(images)
 
 # STEP2: 저장된 이미지를 인코딩 및 이름을 각각 지정 하는 배열
@@ -22,29 +23,30 @@ known_face_names = []
 
 # STEP3: Loop를 수행 하면서 비교 대상 파일 && 비교 대상 파일을 인코딩 함
 for image in images:
+    file_name = image.split('.')[0]         # 파일 명
+    file_ext = image.split('.')[1]          # 파일 확장자
 
-    # STEP4: 이미지 일 경우만 수행을 한다.
-    if image.split('.')[1] == 'png' or 'jpeg' or 'jpg':
+    # STEP4: 디렉토리 내에 이미지인 경우만 수행을 한다.
+    if file_ext == 'png' or file_ext == 'jpeg' or file_ext == 'jpg':
 
         # STEP 4-1: 각각의 파일을 하나씩 가져옴
         current_image = face_recognition.load_image_file("images/" + image)
 
+        # STEP 5: 불러 온 사진의 랜드마크를 찍어서 얼굴 탐지 여부를 체크
         face_landmarks_list = face_recognition.face_landmarks(current_image)
 
-        # 얼굴을 탐지한 경우 - 얼굴의 랜드마크가 존재하는 경우
+        # STEP 6: 얼굴을 탐지한 경우 - 얼굴의 랜드마크가 존재 하는 경우
         if len(face_landmarks_list) > 0:
-            # STEP 3-2: 이미지 인코딩
+            # STEP 6-1: 이미지 인코딩
             current_image_encoded = face_recognition.face_encodings(current_image)[0]
-
-            print(current_image_encoded)
-            # STEP 3-3: 등록된 사용자 이미지 (인코딩 값)를 배열에 넣음
+            # STEP 6-2: 등록된 사용자 이미지 (인코딩 값)를 배열에 넣음
             known_face_encodings.append(current_image_encoded)
-            # STEP 3-3: 등록된 사용자 이름을 배열에 넣음
-            known_face_names.append(image.split('.')[0])
+            # STEP 6-3: 등록된 사용자 이름을 배열에 넣음
+            known_face_names.append(file_name)
         else:
-            print("얼굴을 탐지 못하였습니다 ::: ", image.split('.')[0])
+            print(" ============== 얼굴을 탐지 못 하였습니다 ==============> ", image)
     else:
-        print("이미지 파일이 아닙니다.")
+        print("============== 이미지 파일이 아닙니다.==============")
 
 
 # 변수 초기화
@@ -71,6 +73,7 @@ while True:
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
+
         # 등록된 이미지(known_face_encodings)와 스트림으로 찍히는 얼굴(face_encoding)을 비교한다.
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
